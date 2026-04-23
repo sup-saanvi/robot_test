83c2eeaff822bb6ec54162014b169872fa738894
Abby is good! <saanvip904@gmail.com>
2026-04-23 11:25:15 -0700

    Added a function
    I added a function where I greet the user and ask it to type in one of the options of location they can send their robot to.

--- main.py
+++ main.py
@@ -5,6 +5,39 @@ delivers certain things.
 
 # make a main function that will store the following options for delivery
 # and those are Downtown, Suberbs, and Industrials 
+# get the total delivery distance that is inbetween 5-500 km
+
+# get the weight of the cargo that each robot is carring that's in between 1and 50 kg
+
+# get the weather conditions to see if its clear, rain, or storm 
+
+# check if weight is over 50 kg or if the distance is more than 300 km or if the weather 
+# is storm if it is then print deploment unsafe !
+
+# If it is safe then put down a summary of the weather, zone, and weight print 
+# robots ready for delivery!
+
+def delivery_rout():
+    """Ask user for what location they want to send there robot to and return it."""
+    return input("enter a location to send robot to: ")
+
+name = input("Hello user welcome to the robot delivery game!: ").strip().title()
+    
+print(f"Hello {name}!")
+
+
+def downtown():
+    """"""
+
+
+
+
+
+
+
+
+
+
 
 def main():
     """This is where I will add functions """
@@ -41,14 +74,3 @@ main()
 
 
 
-# get the total delivery distance that is inbetween 5-500 km
-
-# get the weight of the cargo that each robot is carring that's in between 1and 50 kg
-
-# get the weather conditions to see if its clear, rain, or storm 
-
-# check if weight is over 50 kg or if the distance is more than 300 km or if the weather 
-# is storm if it is then print deploment unsafe !
-
-# If it is safe then put down a summary of the weather, zone, and weight print 
-# robots ready for delivery!

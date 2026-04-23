e4e8810d59ab918dff759fd7e64eb1d16c4493a6
Abby is good! <saanvip904@gmail.com>
2026-04-23 10:51:47 -0700

    created sudo code
    I wrote sudo code so then I know what the code should do in plain english and so it will help me do my actual code.

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+This is a game where there is a robot delivery team that
+delivers certain things.
+"""
+
+# make a main function that will store the following options for delivery
+# and those are Downtown, Suberbs, and Industrials 
+
+# get the total delivery distance that is inbetween 5-500 km
+
+# get the weight of the cargo that each robot is carring that's in between 1and 50 kg
+
+# get the weather conditions to see if its clear, rain, or storm 
+
+# check if weight is over 50 kg or if the distance is more than 300 km or if the weather 
+# is storm if it is then print deploment unsafe !
+
+# If it is safe then put down a summary of the weather, zone, and weight print 
+# robots ready for delivery!
